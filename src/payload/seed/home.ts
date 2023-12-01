e1d63f22cbdb3a49c36cd752dd5206d04c0d8785
Eray Demircioğlu <c.eray@hotmail.com>
2023-12-01 22:38:01 +0300

    Ecommerce

--- src/payload/seed/home.ts
+++ src/payload/seed/home.ts
@@ -5,8 +5,8 @@ export const home: Partial<Page> = {
   slug: 'home',
   _status: 'published',
   meta: {
-    title: 'Payload E-Commerce Template',
-    description: 'An open-source e-commerce store built with Payload and Next.js.',
+    title: 'Tech Haven',
+    description: 'Tech Haven',
     image: '{{PRODUCT1_IMAGE}}',
   },
   hero: {
@@ -15,7 +15,7 @@ export const home: Partial<Page> = {
       {
         children: [
           {
-            text: 'Payload E-Commerce Store',
+            text: 'Tech Haven',
           },
         ],
         type: 'h1',
